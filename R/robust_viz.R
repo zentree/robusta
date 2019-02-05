eb9b9d237b0833a276bb58c90fc697e1bb62a8c4
Luis A. Apiolaza <Luis.Apiolaza@canterbury.ac.nz>
2019-02-06 00:45:28 +1300

    Skeleton identifying top_n genotypes.

--- R/robust_viz.R
+++ R/robust_viz.R
@@ -1,5 +1,8 @@
 ## Visualization of rankings for top-n selections
-top_n <- 50
+n_top_candidates <- 50
 non_robust <- TRUE
 
 ## Subset top-n trees from candidates_rank and use sperm plot
+candidates_choices %>%
+  select(Ortet) %>%
+  filter(row_number() <= n_top_candidates) -> id_top_candidates
