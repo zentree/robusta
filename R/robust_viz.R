19670938ca8489c1acea06c9d538bdf39fa1d62e
Luis Apiolaza <lap44@canterbury.ac.nz>
2019-02-08 13:32:37 +1300

    Added zoomed version of rankings.

--- R/robust_viz.R
+++ R/robust_viz.R
@@ -12,6 +12,7 @@ ranking_distrib <- data.frame(ortet = rep(rownames(ranks_top_candidates), each =
                               rankings = as.vector(t(ranks_top_candidates)))
 ranking_distrib$ortet <- with(ranking_distrib, fct_relevel(ortet, id_top_candidates))
 
+# Plot of top candidates, including full distribution of results
 ggplot(ranking_distrib, aes(x = ortet, y = rankings)) +
   geom_point(size = 0.5, alpha = 0.2) +
   stat_summary(fun.y = 'median', geom = 'point', size = 1, colour = 'red') +
@@ -22,3 +23,12 @@ ggplot(ranking_distrib, aes(x = ortet, y = rankings)) +
 
 ggsave('./reports/top-ranking.pdf')
 
+# Zooming in top half of distribution for top candidates
+ggplot(ranking_distrib, aes(x = ortet, y = rankings)) +
+  geom_point(size = 0.5, alpha = 0.2) +
+  stat_summary(fun.y = 'median', geom = 'point', size = 1, colour = 'red') +
+  scale_y_reverse(limits = c(threshold/2, 0)) + theme_minimal() +
+  labs(x = 'Ortet code', y = 'Rankings over 1,000 scenarios') +
+  theme(axis.text.x=element_text(angle=-90))
+
+ggsave('./reports/top-ranking-zoomed.pdf')
