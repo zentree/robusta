407a505969d4a8bc05df3be6cfef2bbe0bcaa424
Luis Apiolaza <lap44@canterbury.ac.nz>
2019-02-08 13:13:08 +1300

    Fixed matrix to vector transformation by transposing it first, converting then by row instead of by column.

--- R/robust_viz.R
+++ R/robust_viz.R
@@ -1,12 +1,24 @@
+library(forcats)
+
 ## Visualization of rankings for top-n selections
-n_top_candidates <- 50
+n_top_candidates <- 30
 
 ## Subset top-n trees from candidates_rank and use sperm plot
 id_top_candidates <- candidates_choices$Ortet[1:n_top_candidates]
 
-ranks_top_candidates <- candidates_rank[candidates_choices$Ortet %in% id_top_candidates,]
-ranking_distrib <- data.frame(Ortet = rep(id_top_candidates, each = nsim),
-                              rankings = matrix(ranks_top_candidates,
-                                                nrow = nsim*n_top_candidates, ncol = 1, byrow = TRUE))
+ranks_top_candidates <- candidates_rank[rownames(candidates_rank) %in% id_top_candidates,]
 
-ggplot(ranking_distrib, aes(x = Ortet, y = rankings)) + geom_point(size = 0.5, alpha = 0.2)
+ranking_distrib <- data.frame(ortet = rep(rownames(ranks_top_candidates), each = nsim),
+                              rankings = as.vector(t(ranks_top_candidates)))
+ranking_distrib$ortet <- with(ranking_distrib, fct_relevel(ortet, id_top_candidates))
+
+ggplot(ranking_distrib, aes(x = ortet, y = rankings)) +
+  geom_point(size = 0.5, alpha = 0.2) +
+  stat_summary(fun.y = 'median', geom = 'point', size = 1, colour = 'red') +
+  geom_hline(yintercept = threshold, colour = 'blue') +
+  scale_y_reverse() + theme_minimal() +
+  labs(x = 'Ortet code', y = 'Rankings over 1,000 scenarios') +
+  theme(axis.text.x=element_text(angle=-90))
+
+ggsave('./reports/top-ranking.pdf')
+
